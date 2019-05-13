75008d979df8ee05d20629b8919aa6a3f3ec0944
hacklock <jiquitaiasoftware@gmail.com>
2019-05-13 20:06:14 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/wiltonribeiro/clientemongodb/ClientemongodbApplication.java
@@ -0,0 +1,13 @@
+package com.wiltonribeiro.clientemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientemongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientemongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiltonribeiro/clientemongodb/ClientemongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wiltonribeiro.clientemongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientemongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
